ac44cabaf0ed1db0ae7ed70165f5119f818ff1d8
Pollyana <pollyanarocha416@gmail.com>
2022-05-30 21:26:03 -0300

    Bit wise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numCombitwiseAdireita, numCombitwiseAesquerda;
+            num = 10;
+            numCombitwiseAdireita = num >> 1;
+            numCombitwiseAesquerda = num << 1;
+            Console.WriteLine("Valor antes: "+num+", valor de num com o bitwise a direita "+numCombitwiseAdireita);
+            Console.WriteLine("Valor antes: "+num+", valor de num com o bitwise a esquerda "+numCombitwiseAesquerda);
+            
+        }
+    }
+}
